3090749b9d6e899f2af877ccb7946e4fb954a11a
Seungwan <mukho@kakao.com>
2022-07-24 21:27:52 +0900

    Swtich

---
+++ switch.js
@@ -0,0 +1,21 @@
+// if ... else if ... else
+let cmd = 3;
+
+switch(cmd){
+    case 0: 
+        console.log(0);
+        break;
+    case 1:
+        console.log(1);
+        break;
+    case 2:
+        console.log(2);
+        break;
+    case 3:
+    case 4:
+        console.log('3~4');
+        break;
+    default:
+        console.log('몰?루');
+        break;
+}
